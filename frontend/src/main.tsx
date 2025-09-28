36c43bbe6b8ae937b8604657bb156ad3f734b627
RAFAEL BRANCO TOTINO <71521653+Rafael-Totino@users.noreply.github.com>
2025-09-28 03:35:44 -0300

    feat: add theme toggle and polish layout styling

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,19 +6,22 @@ import App from './App';
 import { AuthProvider } from './context/AuthContext';
 import { ChatProvider } from './context/ChatContext';
 import { RecipeProvider } from './context/RecipeContext';
+import { ThemeProvider } from './context/ThemeContext';
 
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
-      <AuthProvider>
-        <RecipeProvider>
-          <ChatProvider>
-            <App />
-          </ChatProvider>
-        </RecipeProvider>
-      </AuthProvider>
+      <ThemeProvider>
+        <AuthProvider>
+          <RecipeProvider>
+            <ChatProvider>
+              <App />
+            </ChatProvider>
+          </RecipeProvider>
+        </AuthProvider>
+      </ThemeProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
